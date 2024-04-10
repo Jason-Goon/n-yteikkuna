68f222edb40b6d9c822342a496301457713e20c4
Jason-Goon <156106640+Jason-Goon@users.noreply.github.com>
2024-04-10 22:12:50 +0300

    Update palette.js

--- showcase/src/scripts/palette.js
+++ showcase/src/scripts/palette.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     function applyPalette(paletteName) {
         const paletteColors = {
-            // Define your palettes here
             palette1: ['#186F65', '#B5CB99', '#FCE09B', '#3D0C11', '#3B2533E'],
             palette2: ['#1D2B53', '#7E2553', '#FF004D', '#F6D776', '#FAEF5D'],
             palette3: ['#FF8080', '#FFCF96', '#F6FDC3', '#CDFADB', '#0D9276'],
             // Default palette
-            defaultPalette:  ['#186F65', '#B5CB99', '#FCE09B', '#B2533E', '#3D0C11']// Example default colors
-        }[paletteName || 'defaultPalette']; // Use 'defaultPalette' if no paletteName is provided
+            defaultPalette:  ['#186F65', '#B5CB99', '#FCE09B', '#B2533E', '#3D0C11']
+        }[paletteName || 'defaultPalette']; 
 
         if (paletteColors) {
             document.documentElement.style.setProperty('--primary-color', paletteColors[0]);
@@ -15,16 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
             document.documentElement.style.setProperty('--tertiary-color', paletteColors[2]);
             document.documentElement.style.setProperty('--quaternary-color', paletteColors[3]);
             document.documentElement.style.setProperty('--quinary-color', paletteColors[4]);
-            // Update body background and text color based on the selected or default palette
             document.documentElement.style.setProperty('--body-bg-color', paletteColors[0]);
             document.documentElement.style.setProperty('--text-color', paletteColors[4]);
-            
-            // Save the selected or default palette name for persistence
             localStorage.setItem('selectedPalette', paletteName || 'defaultPalette');
         }
     }
 
-    // Attempt to apply a saved theme or the default on page load
     const savedPalette = localStorage.getItem('selectedPalette');
     applyPalette(savedPalette);
 
